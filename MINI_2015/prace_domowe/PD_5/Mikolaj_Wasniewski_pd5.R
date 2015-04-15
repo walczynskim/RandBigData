8b25506ea7535573f70649fddf78740a340573ae
wasniewskim <wasniewskim@student.mini.pw.edu.pl>
2015-04-15 23:40:38 +0200

    Mikołaj Waśniewski pd5

---
+++ MINI_2015/prace_domowe/PD_5/Mikolaj_Wasniewski_pd5.R
@@ -0,0 +1,32 @@
+haslo <- '64tl1bp6un9sqxp8'
+
+library(RMySQL)
+library(stringi)
+library(dplyr)
+
+haslo<-stri_reverse(haslo)
+
+# wczytanie pliku z danymi do ramki danych:
+Mikolaj_Wasniewski_dane<-read.table("C:/Users/Miko³aj/Documents/SMAD/R and Big Data/wybory/d/a2015-04-06_10_23_28.txt")
+
+
+polaczenie <- src_mysql(dbname = 'students', 
+                        host = 'beta.icm.edu.pl', user = 'pbiecek', 
+                        password = haslo)
+
+
+uchwyt_doTabeli <- copy_to(polaczenie, Mikolaj_Wasniewski_dane, 
+                           temporary = FALSE)
+
+
+# sprawdzenie jakie tabele sa w bazie danych:
+sterownik <- MySQL()
+mpolaczenie = dbConnect(sterownik, 
+                        user='pbiecek', password=haslo, dbname='students', 
+                        host='beta.icm.edu.pl')
+
+dbListTables(mpolaczenie)
+
+dbDisconnect(mpolaczenie)
+
+
