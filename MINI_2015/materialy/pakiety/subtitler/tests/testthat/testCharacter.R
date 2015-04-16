150bb97024c5030053d36b0d5b04f1efde61d39a
pbiecek <przemyslaw.biecek@gmail.com>
2015-04-16 22:43:53 +0200

    testy

--- MINI_2015/materialy/pakiety/subtitler/tests/testthat/testCharacter.R
+++ MINI_2015/materialy/pakiety/subtitler/tests/testthat/testCharacter.R
@@ -1,9 +1,10 @@
 test_that("result is of the character class", {
-  expect_equal(class(readSubtitle("http://dl.opensubtitles.org/pl/download/file/1954081967")), "character")
+  expect_equal(class(readSubtitle("http://dl.opensubtitles.org/pl/download/file/1954081967")),
+               "character")
 })
 
 test_that("result is of the character class", {
-  expect_equal(class(readSubtitle("http://dl.opensubtitles.org/pl/download/file/1954081967")), "character")
+  expect_equal(readSubtitle("http://dl.opensubtitles.org/pl/download/file/1954081967")[1], "1")
 })
 
 test_that("result is of the character class", {
