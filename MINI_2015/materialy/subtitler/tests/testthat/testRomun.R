b3d8c7c636e5345b438c824cfcd45ce8e31f1058
grabarze <grabowskip@student.mini.pw.edu.pl>
2015-04-24 18:20:13 +0200

    poprawa

--- MINI_2015/materialy/subtitler/tests/testthat/testRomun.R
+++ MINI_2015/materialy/subtitler/tests/testthat/testRomun.R
@@ -1,6 +1,12 @@
-test_that("checking if character",
-  expect_equal(class(OstatnieRomunskieNapisy()), 'character'))
+test_that("checking if return data frame",
+  expect_equal(class(OstatnieRomunskieNapisy(2014, 8.8, FALSE, TRUE)), 'data.frame'))
 
-test_that("testing if length equals to 40",
-          expect_equal(length(OstatnieRomunskieNapisy()), 40))
+test_that("testing return data frame for data that would give no output",
+          expect_equal(class(OstatnieRomunskieNapisy(3000, 10, FALSE, FALSE)), 'data.frame'))
 
+
+test_that("testing if makes .txt file",
+          {
+            OstatnieRomunskieNapisy(2013, 9.0, fileSave = TRUE)
+            file.exists('OstatnieRomunskieNapisy.txt')
+          } == TRUE)
